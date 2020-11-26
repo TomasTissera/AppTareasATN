5270ef3dade3eff2d7b19135d3e341d75118e188
Tomás Tissera <tomastissefa@gmail.com>
2020-11-26 13:15:45 -0300

    app

--- lib/pages/pageSearch.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class PageSearch extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text("Page Search"),
-    );
-  }
-}
--- lib/pages/pagelist.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class PageList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text("Page List"),
-    );
-  }
-}
